b2fe31cf8a4bf64b414d5dacd6494fbb62b1474a
Guyliann <engels.guyliann@gmail.com>
2018-04-26 16:18:25 +0200

    analyse des nutriments : suite

--- R/nutri_aa3.R
+++ R/nutri_aa3.R
@@ -6,7 +6,16 @@ repos_load(file = "Data/ecophysio2018/aa3/180425A-inorga_2018-04-25_13.48.23_5AD
 
 nutri <- EcoNumData_aa3.inorga
 rm(EcoNumData_aa3.inorga)
-nutri <- filter(nutri, sample_type == "SAMP")
+samp <- filter(nutri, sample_type == "SAMP")
+
+repos_load(file = "Data/ecophysio2018/aa3/180426A-inorga_2018-04-26_11.00.22_5AE11680_aa3.RData")
+
+nutri <- EcoNumData_aa3.inorga
+rm(EcoNumData_aa3.inorga)
+samp1 <- filter(nutri, sample_type == "SAMP")
+
+nutri <- bind_rows(samp, samp1)
+rm(samp, samp1)
 
 nutri$sample_date <- as.POSIXct(nutri$sample_date)
 nutri <- separate(nutri, col = sample, into = c("localisation", "position", "cycle"), sep = "-",remove = FALSE)
